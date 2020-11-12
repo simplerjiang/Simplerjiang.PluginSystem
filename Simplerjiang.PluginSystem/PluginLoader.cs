a8a2a39663ca7fd797d49208b9a39f321b980449
贤龄 江 <jiangsimpler@gmail.com>
2020-11-12 18:36:28 +0800

    添加项目文件。

---
+++ Simplerjiang.PluginSystem/PluginLoader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Simplerjiang.PluginSystem
+{
+     public class PluginsLoader
+    {
+        public PluginsLoader()
+        {
+            pluginsPath = GetPluginsPath();
+        }
+
+        public PluginsLoader(string specialPath)
+        {
+            pluginsPath = specialPath;
+        }
+
+        private string pluginsPath;
+
+        private string GetPluginsPath()
+        {
+            return Path.Combine(Directory.GetCurrentDirectory(), "Plugins");
+        }
+
+        private FileInfo[] GetAllPluginsPath()
+        {
+            var files = new DirectoryInfo(pluginsPath).GetFiles();
+            return files;
+        }
+
+        public List<Type> LoadPlugin()
+        {
+            var types = new List<Type>();
+            foreach (var file in GetAllPluginsPath())
+            {
+                var assembly = Assembly.LoadFile(file.FullName);
+                types.AddRange(assembly.GetTypes());
+            }
+            return types;
+        }
+    }
+}
